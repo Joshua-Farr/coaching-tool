e385ef62d6b522ad8161613167c2a26345e424ad
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2025-07-14 17:27:54 -0500

    Hooked into OpenAPI

--- server/server.js
+++ server/server.js
@@ -1,23 +1,33 @@
 import OpenAPI from "openai";
+import express from "express";
 
-const express = require("express");
-const openai = new OpenAPI();
-const PORT = 8080;
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-
-app.get("/api", (req, res) => {
-  const response = await openai.responses.create(
-    {
-        model: "gpt-4o-mini",
-        input: req.body,
-        store: true,
-    }
-  )
-
-  res.json({ reponse: response.output_text });
+const openai = new OpenAPI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+const port = process.env.PORT;
+
+app.get("/api", async (req, res) => {
+  //   res.json({ reponse: response.output_text });
+  const gptResponse = await main("Write a one sentence story about a unicorn.");
+  console.log("Here is your response", gptResponse);
+  res.json({ response: gptResponse || "No response generated from ChatGPT" });
 });
+
+app.listen(port, () => {
+  console.log(`Server started on  http://localhost:${port}`);
+});
+
+async function main(text) {
+  const response = await openai.responses.create({
+    model: "gpt-4o-mini",
+    input: text,
+    store: true,
+  });
+
+  return response;
+}
